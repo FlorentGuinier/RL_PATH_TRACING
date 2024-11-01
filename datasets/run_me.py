b8b62768c56b2ea8d65ae2f67666350cb72dd2af
Florent Guinier <florent.guinier@epicgames.com>
2024-11-01 11:13:42 +0100

    Improve data generation script (logs and progress bar)

--- datasets/run_me.py
+++ datasets/run_me.py
@@ -3,12 +3,13 @@ import os
 path = "D:/GitRepos/rl-path-tracing/datasets/SunTemple_v4/SunTemple/SunTemple.blend"
 #todo other scene and additional frames
 #len = 1201
-len = 5
+len = 60
 
 import torch
 import numpy as np
 import os
 import minexr
+from tqdm import tqdm
 
 def store_flow(path,out):    
     with open(path, 'rb') as fp:
@@ -67,19 +68,18 @@ def store_flow(path,out):
     #aa = torch.Tensor(np.tile(np.arange(a).T,(720,1)).T)
     #data[:,:,0] = torch.Tensor((data[:,:,0]*(2)+bb*2-b+1)/b)
     #data[:,:,1] = torch.Tensor((-data[:,:,1]*(2) +aa*2-a+1)/a)
-
-    print(path)
+    #print(path)
 
 motion_vector_file_path = 'd:/rl_dataset/add/Motion'
 
-for i in range(0,len):
-    print("* generate_gd: individual samples and ground truth ******************************")
-    os.system("blender "+ path +" --background --python generate_gd.py -- " +str(i) )
+for i in (pbar := tqdm(range(0,len), desc="Frame")):
+    pbar.set_description(f"Processing {i} (Ground truth and samples)")
+    os.system("blender "+ path +" --background --python generate_gd.py -- " +str(i) + " > logs/generate_gd"+str(i)+".txt")
 
-    print("* generate_add: normal, depth, albedo and motion vector as exr ******************")
-    os.system("blender "+ path +" --background --python generate_add.py -- " +str(i))
+    pbar.set_description(f"Processing {i} (Additional buffers)")
+    os.system("blender "+ path +" --background --python generate_add.py -- " +str(i) + " > logs/generate_add"+str(i)+".txt")
 
-    print("* convert motion vector to torch format *****************************************")
+    pbar.set_description(f"Processing {i} (Motion vector convertion)")
     frame_number_str = str(i).zfill(4)
     store_flow(motion_vector_file_path+frame_number_str+".exr",motion_vector_file_path+frame_number_str+".pt")
     
